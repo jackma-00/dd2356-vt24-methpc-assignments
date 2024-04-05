8aae49a89aea5b6524ee74ea27f18cc980def0e9
jackma-00 <jacopo.maragna1@gmail.com>
2024-04-05 11:08:14 +0200

    MSIZE 64 Optimized

--- assignments/hw1/exer5/matrix_multiply.c
+++ assignments/hw1/exer5/matrix_multiply.c
@@ -34,11 +34,11 @@ void initialize_matrices()
 
 void multiply_matrices()
 {
-  int i, j, k;
- 
+  int i, j, k ;
+  // Loop nest optimized algorithm
   for (i = 0 ; i < MSIZE ; i++) {
-    for (j = 0 ; j < MSIZE ; j++) {
-      for (k = 0 ; k < MSIZE ; k++) {
+    for (k = 0 ; k < MSIZE ; k++) {
+      for (j = 0 ; j < MSIZE ; j++) {
         matrix_r[i][j] += matrix_a[i][k] * matrix_b[k][j];
       }
     }
