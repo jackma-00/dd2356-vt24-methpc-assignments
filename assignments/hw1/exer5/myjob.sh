90dd5b637df437e4c12d08c2546fa8b833865d93
jackma-00 <jacopo.maragna1@gmail.com>
2024-04-05 10:18:55 +0200

    Update project allocation in myjob.sh

---
+++ assignments/hw1/exer5/myjob.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+# The -l above is required to get the full environment with modules
+
+# The name of the script is myjob
+#SBATCH -J myjob
+# Only 1 hour wall-clock time will be given to this job
+#SBATCH -t 1:00:00
+#SBATCH -A edu24.DD2356
+# Number of nodes
+#SBATCH -p shared
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --nodes=1
+#SBATCH -e error_file.e
+
+# Run the executable file 
+# and write the output into my_output_file
+srun -n 1 ./matrix_multiply.out > matrix_multiply_output.txt
