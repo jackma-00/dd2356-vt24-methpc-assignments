4ad5febf587f74014d56b66a926084c35bdaaf27
jackma-00 <jacopo.maragna1@gmail.com>
2024-04-05 15:15:36 +0200

    Add transposeBase and transposeCacheBlocked functions to transpose.c

--- assignments/hw1/exer5/transpose/transpose.c
+++ assignments/hw1/exer5/transpose/transpose.c
@@ -9,77 +9,91 @@ double a[N][N];
 double b[N][N];
 
 // timer
-double mysecond(){
+double mysecond()
+{
   struct timeval tp;
   struct timezone tzp;
   int i;
 
-  i = gettimeofday(&tp,&tzp);
-  return ( (double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
+  i = gettimeofday(&tp, &tzp);
+  return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
 }
 
 // basic transposition without code transformations
-void transposeBase(){
+void transposeBase()
+{
   int i, j;
 
   for (i = 0; i < N; ++i)
     for (j = 0; j < N; ++j)
       b[i][j] = a[j][i];
-
 }
 
+void transposeCacheBlocked()
+{
+  const int STRIP = 256;
+  for (int ii = 0; ii < N; ii += STRIP)
+    for (int jj = 0; jj < N; jj += STRIP)
+      for (int i = ii; i < ii + STRIP; i++)
+        for (int j = jj; j < jj + STRIP; j++)
+        {
+          b[i][j] = a[j][i];
+        }
+}
 
-void initialize_matrices(){
-  int i, j ;
- 
-  for (i = 0 ; i < N ; i++) {
-    for (j = 0 ; j < N ; j++) {
-      a[i][j] = (double) rand() / RAND_MAX;
-      b[i][j] = (double) rand() / RAND_MAX;
+void initialize_matrices()
+{
+  int i, j;
+
+  for (i = 0; i < N; i++)
+  {
+    for (j = 0; j < N; j++)
+    {
+      a[i][j] = (double)rand() / RAND_MAX;
+      b[i][j] = (double)rand() / RAND_MAX;
     }
   }
-
-
 }
 
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-    
-    int maxTest=10;
-    int i, j, tests;
-    int k, rept=10;
-    double tStart, tEnd;
-    double tLoop, t, rate;
 
-    
-    printf( "Transpose %d x %d\n", N, N );
+  int maxTest = 10;
+  int i, j, tests;
+  int k, rept = 10;
+  double tStart, tEnd;
+  double tLoop, t, rate;
 
-    initialize_matrices();
-    
-    // Start tests
-    tLoop = 1.0e10; // take a large number
-    for (tests=0; tests<maxTest; tests++) {
-	     
-       tStart = mysecond();
-	     
-       for (k=0; k<rept; k++) {
-	        transposeBase();
-	     }
-       
-       tEnd = mysecond();
-	     t =  tEnd - tStart;
-	     t = t / rept;
-       // record the best time for all the different tests
-	     if (t < tLoop) tLoop = t;
+  printf("Transpose %d x %d\n", N, N);
+
+  initialize_matrices();
+
+  // Start tests
+  tLoop = 1.0e10; // take a large number
+  for (tests = 0; tests < maxTest; tests++)
+  {
+
+    tStart = mysecond();
+
+    for (k = 0; k < rept; k++)
+    {
+      transposeBase();
     }
-    rate = 8*(N*N) / tLoop;
-    printf( "BaseTime = %.2e s, Base Rate = %.2e MB/s\n", tLoop, rate*1.0e-6);
 
-    // reuse b to avoid smart compiler issues
-    for (i = 0 ; i < N ; i++) 
-      for (j = 0 ; j < N ; j++) 
-        b[i][j]++;
-    
+    tEnd = mysecond();
+    t = tEnd - tStart;
+    t = t / rept;
+    // record the best time for all the different tests
+    if (t < tLoop)
+      tLoop = t;
+  }
+  rate = 8 * (N * N) / tLoop;
+  printf("BaseTime = %.2e s, Base Rate = %.2e MB/s\n", tLoop, rate * 1.0e-6);
 
-    return 0;
+  // reuse b to avoid smart compiler issues
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++)
+      b[i][j]++;
+
+  return 0;
 }
