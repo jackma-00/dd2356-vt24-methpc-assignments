81b1b0a762680f314b1bfa2f70ce0252b79d47a7
michailroussos <michael_roussos@yahoo.com>
2024-04-10 21:31:53 +0200

    small change in hw2_ex1

--- assignments/hw2/exer1/openmp_hello_world.c
+++ assignments/hw2/exer1/openmp_hello_world.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 #include <omp.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: ./program_name <num_threads>\n");
-        return 1;
-    }
-
-    int num_threads = atoi(argv[1]);
-
-    #pragma omp parallel num_threads(num_threads)
+int main() {
+    #pragma omp parallel
     {
         int thread_id = omp_get_thread_num();
         printf("Hello World from Thread %d!\n", thread_id);
