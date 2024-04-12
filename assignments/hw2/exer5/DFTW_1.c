6f7c18f253660517af5cd9a26afe651f3dd36031
jackma-00 <jacopo.maragna1@gmail.com>
2024-04-12 14:29:59 +0200

    Add performance testing for different number of threads in DFTW_1.c

--- assignments/hw2/exer5/DFTW_1.c
+++ assignments/hw2/exer5/DFTW_1.c
@@ -54,11 +54,16 @@ int main(int argc, char *argv[]) {
   double start_time = omp_get_wtime();
 
   // DFT
-  int idft = 1;
-  DFT(idft, xr, xi, Xr_o, Xi_o, N);
-  // IDFT
-  idft = -1;
-  DFT(idft, Xr_o, Xi_o, xr_check, xi_check, N);
+  // Test performance for 1, 32, 64, and 128 threads
+  int number_of_threads[] = {1, 32, 64, 128};
+  for (int i = 0; i < 4; i++) {
+    omp_set_num_threads(number_of_threads[i]); // set number of threads
+    int idft = 1;
+    DFT(idft, xr, xi, Xr_o, Xi_o, N);
+    // IDFT
+    idft = -1;
+    DFT(idft, Xr_o, Xi_o, xr_check, xi_check, N);
+  }
 
   // stop timer
   double run_time = omp_get_wtime() - start_time;
@@ -98,6 +103,33 @@ int DFT(int idft, double *xr, double *xi, double *Xr_o, double *Xi_o, int N) {
 
   // normalize if you are doing IDFT
   if (idft == -1) {
+    for (int n = 0; n < N; n++) {
+      Xr_o[n] /= N;
+      Xi_o[n] /= N;
+    }
+  }
+  return 1;
+}
+
+// DFT/IDFT parallel routine
+// idft: 1 direct DFT, -1 inverse IDFT (Inverse DFT)
+int DFT_parallel(int idft, double *xr, double *xi, double *Xr_o, double *Xi_o,
+                 int N) {
+  printf("Number of threads: %d\n", omp_get_num_threads());
+  #pragma omp for
+  for (int k = 0; k < N; k++) {
+    for (int n = 0; n < N; n++) {
+      // Real part of X[k]
+      Xr_o[k] +=
+          xr[n] * cos(n * k * PI2 / N) + idft * xi[n] * sin(n * k * PI2 / N);
+      // Imaginary part of X[k]
+      Xi_o[k] +=
+          -idft * xr[n] * sin(n * k * PI2 / N) + xi[n] * cos(n * k * PI2 / N);
+    }
+  }
+  // normalize if you are doing IDFT
+  if (idft == -1) {
+    #pragma omp for
     for (int n = 0; n < N; n++) {
       Xr_o[n] /= N;
       Xi_o[n] /= N;
