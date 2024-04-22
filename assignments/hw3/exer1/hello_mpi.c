314a2dee23c990630c3cf4a5307bb005cb73f3be
michailroussos <michael_roussos@yahoo.com>
2024-04-22 16:50:54 +0200

    update in hw3 ex1 to use MPI2

--- assignments/hw3/exer1/hello_mpi.c
+++ assignments/hw3/exer1/hello_mpi.c
@@ -2,10 +2,10 @@
 #include <mpi.h>
 
 int main(int argc, char *argv[]) {
-    int rank, size;
+    int rank, size, provided;
     
     // Initialize MPI environment
-    MPI_Init(&argc, &argv);
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
     
     // Get the rank of the current process
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
